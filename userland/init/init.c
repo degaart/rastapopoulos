787351148966a7ae8958d09b75a6002d993b5eaf
Degaart <degaart@users.noreply.github.com>
2017-08-30 20:26:40 +0300

    Clean up code

--- userland/init/init.c
+++ userland/init/init.c
@@ -13,59 +13,18 @@ void main()
         invalid_code_path();
     }
 
-#if 0
-    /* Start vfs */
-    int vfs_pid = fork();
-    if(!vfs_pid) {
-        exec("vfs.elf");
-        invalid_code_path();
-    }
-#endif
-
-    /* Start our block driver test */
+    /* Start block driver */
     int blockdrv_pid = fork();
     if(!blockdrv_pid) {
         exec("blockdrv.elf");
         invalid_code_path();
     }
 
-    /* Get sector count */
-    uint32_t sector_count = blockdrv_sector_count();
-    trace("Sector count: %d (%d Mb)",
-          sector_count,
-          sector_count / 2048);
-
-    /* Read sectors and calculate crc of whole disk */
-    uint32_t crc = crc_init();
-    char buffer[512];
-    for(uint32_t sector = 0; sector < sector_count; sector++) {
-        if((sector % 1000) == 0)
-            trace("Reading sector %d", sector);
-
-        int ret = blockdrv_read(buffer, sizeof(buffer), sector);
-        assert(ret == 0);
-
-        crc = crc_update(crc, buffer, sizeof(buffer));
+    /* Start vfs */
+    int vfs_pid = fork();
+    if(!vfs_pid) {
+        exec("vfs.elf");
+        invalid_code_path();
     }
-
-    crc = crc_finalize(crc);
-    trace("CRC32: 0x%X", crc);
-
-#if 0
-    /* Try to read from initrd */
-    int fd = open("init.c", O_RDONLY, 0);
-    assert(fd != -1);
-
-    char buffer[512];
-    int ret;
-    while(1) {
-        ret = read(fd, buffer, sizeof(buffer));
-        assert(ret != -1);
-        if(ret == 0)
-            break;
-
-        debug_writen(buffer, ret);
-    }
-#endif
 }
 
