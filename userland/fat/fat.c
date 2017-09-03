69a7ffdfcee99b707cbfeda82e8dd1b8c5250b1d
Degaart <degaart@users.noreply.github.com>
2017-09-03 15:37:02 +0300

    More FAT filesystem exploration code

--- userland/fat/fat.c
+++ userland/fat/fat.c
@@ -12,14 +12,45 @@ void main()
     FATFS fat;
     FRESULT fres;
 
+    /* Mount drive 0 */
     memset(&fat, 0, sizeof(fat));
     fres = f_mount(&fat, "0:/", 1);
     if(fres != FR_OK) {
         panic("mount() failed: %d", fres);
     }
 
+    /* List contents of root dir */
+    DIR hdir;
+    fres = f_opendir(&hdir, "/");
+    if(fres != FR_OK) {
+        panic("opendir() failed: %d", fres);
+    }
+
+    while(true) {
+        FILINFO info;
+
+        fres = f_readdir(&hdir, &info);
+        if(fres != FR_OK) {
+            panic("readdir() failed: %d", fres);
+        } else if(info.fname[0] == '\0') {
+            break;
+        }
+
+        if(info.fattrib & AM_DIR) {
+            trace("[%s]",
+                  info.fname);
+        } else {
+            trace("%s %lld",
+                  info.fname,
+                  (unsigned long long)info.fsize);
+        }
+    }
+
+    f_closedir(&hdir);
+
+    /* read init.c and calculate crc32 */
     FIL hfile;
-    fres = f_open(&hfile, "0:/init.c", FA_READ);
+    fres = f_open(&hfile, "init.c", FA_READ);
     if(fres != FR_OK) {
         panic("open() failed: %d", fres);
     }
